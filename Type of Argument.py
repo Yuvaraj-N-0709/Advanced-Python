18076c6da9b5d6e5b000704c94bb2df35b9be02d
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-06-19 18:15:54 +0530

    9-Type of Argument

---
+++ Type of Argument.py
@@ -0,0 +1,20 @@
+
+#position argument
+"""
+def add(x,y):
+    z=x+y
+    print("sum:",z)
+add(5,10)
+
+def person(name,age):
+    print("name:",name)
+    print("age:",age+2)
+person("yuvaraj",23)
+#person(23,"yuva")
+"""
+
+#keywords
+def person(name,age):
+    print("name:",name)
+    print("age:",age+2)
+person(name="yuvaraj",age=23)
