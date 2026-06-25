22888f83c7f13afb448dfc420b163ac8c3fca4b9
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-06-25 13:52:18 +0530

    19-Constructor

---
+++ Constructor.py
@@ -0,0 +1,46 @@
+#constructor
+#constructor is to initialize(assign value) to the data numbers  of the class when on object of class is created
+#types of constructor
+#1---defalut constructor
+#2---parameterized constructor
+
+"""
+class test1:
+    
+    def __init__(self):
+        print("Hai")
+obj1=test1()
+
+
+class Student:
+    def __init__(self):
+        name="yuvaraj"
+        age=23
+        print("my name is:",name)
+        print("my age is :",age)
+stu=Student()
+
+class Student:
+    def __init__(self):
+        name="yuvaraj"
+        age=23
+        print("my name is:",name)
+        print("my age is :",age)
+stu1=Student()
+stu2=Student()
+
+class person:
+    def __init__(self):
+        print("1")
+    def __init__(self):
+        print("2")
+p1=person()
+"""
+
+class person:
+    def __init__(self):
+        print("1")
+    def __init__(self):
+        print("2")
+p1=person()
+p2=person()
