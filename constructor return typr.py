cc75c81e1aae1f2bb1d483907d76e86207aea14b
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-06-25 13:55:27 +0530

    24-constructor return type

---
+++ constructor return typr.py
@@ -0,0 +1,28 @@
+class add:
+    def a(self,a,b):
+        c=a+b
+        return c
+add1=add()
+add1.a(10,20)
+
+
+class test:
+    def __init__(self):
+        print("Hai")
+        return True
+d=test()
+
+
+class test:
+    def __init__(self):
+        a="yuvaraj"
+        return a
+add1=add(10,20)
+
+
+class person:
+    name="yuva"
+    def __init__(self):
+        name="yuvaraj"
+        print(name)
+obj=person()
