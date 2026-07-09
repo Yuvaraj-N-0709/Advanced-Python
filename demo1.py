39a34bc414cb425512715c51720c3db84a0fd903
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-07-09 13:45:30 +0530

    67-demo1

---
+++ demo1.py
@@ -0,0 +1,16 @@
+#demo1
+"""
+print("demo1:",__name__)
+"""
+
+def greet():
+    print("good morning")
+def message():
+    print("have a good day")
+def main():
+    greet()
+    message()
+if __  name__=="__main__":
+    main()
+print("first:",__name__)
+
